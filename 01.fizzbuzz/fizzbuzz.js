0ffdeab279b28a886a2ab14fe3b6f64a9f486f2d
akiyama <ryo.akiyama112345@gmail.com>
2024-11-19 12:38:42 +0900

    maxNumberはプログラム全体の設定として表示する数の範囲を定義するため独立。fizzBuzzRulesは convertToFizzBuzz 関数で使用される変換ルールのみを含み、その関数に固有の設定であることを明確に

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,26 +1,25 @@
 #!/usr/bin/env node
 
-const CONFIG = {
-  maxNumber: 20,
-  rules: [
-    {
-      condition: (num) => num % 3 === 0 && num % 5 === 0,
-      output: "FizzBuzz",
-    },
-    {
-      condition: (num) => num % 3 === 0,
-      output: "Fizz",
-    },
-    {
-      condition: (num) => num % 5 === 0,
-      output: "Buzz",
-    },
-  ],
-};
+const maxNumber = 20;
+
+const fizzBuzzRules = [
+  {
+    condition: (num) => num % 3 === 0 && num % 5 === 0,
+    output: "FizzBuzz",
+  },
+  {
+    condition: (num) => num % 3 === 0,
+    output: "Fizz",
+  },
+  {
+    condition: (num) => num % 5 === 0,
+    output: "Buzz",
+  },
+];
 
 const convertToFizzBuzz = (number) =>
-  CONFIG.rules.find((rule) => rule.condition(number))?.output ||
+  fizzBuzzRules.find((rule) => rule.condition(number))?.output ||
   number.toString();
 
-const numbers = [...Array(CONFIG.maxNumber)].map((_, index) => index + 1);
+const numbers = [...Array(maxNumber)].map((_, index) => index + 1);
 numbers.forEach((number) => console.log(convertToFizzBuzz(number)));
