0f8747ef1187633a14f94fc77c39a339ffc5359f
akiyama <ryo.akiyama112345@gmail.com>
2024-11-28 16:50:04 +0900

    配列として扱うために一般化したfizzBuzzRulesを削除し、ルールを関数として表現。各条件を独立させることで順序依存を排除。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,24 +2,12 @@
 
 const maxNumber = 20;
 
-const fizzBuzzRules = [
-  {
-    condition: (num) => num % 3 === 0 && num % 5 === 0,
-    output: "FizzBuzz",
-  },
-  {
-    condition: (num) => num % 3 === 0,
-    output: "Fizz",
-  },
-  {
-    condition: (num) => num % 5 === 0,
-    output: "Buzz",
-  },
-];
-
-const convertToFizzBuzz = (number) =>
-  fizzBuzzRules.find((rule) => rule.condition(number))?.output ??
-  number.toString();
+const convertToFizzBuzz = (number) => {
+  let result = "";
+  if (number % 3 === 0) result += "Fizz";
+  if (number % 5 === 0) result += "Buzz";
+  return result || number.toString();
+};
 
 const numbers = [...Array(maxNumber)].map((_, index) => index + 1);
 numbers.forEach((number) => {
